6e87918d056b4ea1c5d15e2fd5b1d09a84ca909a
Yose <136212888+Yose1001@users.noreply.github.com>
2026-04-28 01:17:24 +0700

    fix: cors and remove port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST'],
 }));
 app.use(express.json());
